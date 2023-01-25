2aeaf099bbbb804e9d2627145f6105ad472ef97d
Daniel <dursuaf@gmail.com>
2023-01-25 20:35:39 +0100

    Update functions

---
+++ functions/functions.js
@@ -0,0 +1,25 @@
+const arrayLength = (array) => {
+  let i = 0;
+  let length = 0;
+  while (array[i] !== undefined) {
+    if (array[i]) {
+      i++;
+      length++;
+    }
+  }
+  return length;
+};
+
+const arrayPush = (array, item) => {
+  const x = arrayLength(array);
+  [];
+  array[x] = item;
+  return x;
+};
+
+const arrayPop = (array) => {
+  const x = arrayLength(array);
+  const y = x - 1;
+  array[y] = undefined;
+  return array[x];
+};
